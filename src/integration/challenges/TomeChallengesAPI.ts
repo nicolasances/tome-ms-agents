0727d049d18c66979c1fa7531933228c007c4e6c
nicolasances <nicolas.matteazzi@gmail.com>
2025-12-14 17:06:48 +0100

    Fixed integration with Challenges API

--- src/integration/challenges/TomeChallengesAPI.ts
+++ src/integration/challenges/TomeChallengesAPI.ts
@@ -1,11 +1,10 @@
-import { TotoAPI, TotoAPIRequest } from "toto-api-controller";
+import { RegistryCache, TotoAPI, TotoAPIRequest } from "toto-api-controller";
 import { TomeChallenge } from "./model/TomeChallenge";
 
 export class TomeChallengesAPI extends TotoAPI {
 
     async saveChallenge(challenge: TomeChallenge, cid: string): Promise<SaveChallengeResponse> {
-
-        return this.post(new TotoAPIRequest('/challenges', cid), SaveChallengeResponse)
+        return this.post(new TotoAPIRequest('/challenges', challenge, cid), SaveChallengeResponse)
     }
 
 }
